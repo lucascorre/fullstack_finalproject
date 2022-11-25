8cff44ae41316f2f6ef1cb7e664434a30a439350
Louis JEAN <spideai@outlook.fr>
2022-11-25 16:30:04 +0100

    git vomité

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import AppLayout from "../src/components/layouts/AppLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout>
+     <Component {...pageProps} />
+    </AppLayout>
+    )
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './Navbar.module.scss';
+import Button from "../common/button/Button";
+
+function Navbar() {
+    return (
+        <div className={classes.navBar}>
+            <div className={classes.nameAndLogo}>
+                <img className={classes.logo} src="Logo.svg" alt="logo"/>
+                <h1 className={classes.name}>RIDE</h1>
+            </div>
+            <div className={classes.links}>
+                <Button title="Connexion admin" />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
