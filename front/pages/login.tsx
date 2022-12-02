64078c89bea31fe1f30e262c146ea49df59314f6
Louis JEAN <spideai@outlook.fr>
2022-12-02 19:30:41 +0100

    Merge branch 'main' of github.com:lucascorre/fullstack_finalproject
    Signed-off-by: Louis JEAN <spideai@outlook.fr>
    
    # Conflicts:
    #	front/src/components/common/inputs/TextInput.tsx

--- front/pages/login.tsx
+++ front/pages/login.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import classes from "./login.module.scss";
 
 export const Login = () => {
-  return(
-    <div className={classes.loginPage}>
-      <div className={classes.loginContainer}>
-        <h2>CONNEXION</h2>
-          <form>
-
-          </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className={classes.loginPage}>
+            <div className={classes.loginContainer}>
+                <h2>CONNEXION</h2>
+                <div className={classes.form}>
+                    <form>
+                        {/*<TextInput title={"Identifiant"}/>*/}
+                        {/*<TextInput title={"Mot de passe"}/>*/}
+                        {/*<Button title={"Connexion"}/>*/}
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Login
